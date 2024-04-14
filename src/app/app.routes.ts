e33a5bdfc74e020baa7ec1beec9d70de7c50baeb
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-14 05:20:19 -0700

    add jwt decode to main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+ 
+];
